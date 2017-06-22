1c74a0c50c7e1d81622529d6cdf07716fa3d6842
shefali1310 <yshefali.04@gmail.com>
2017-06-23 00:09:28 +0530

    Delete love.py

--- love.py
+++
@@ -1,8 +0,0 @@
-print "hello"
-spy_name = raw_input("Enter your name")
-print "Welcome " + spy_name + ". Glad to meet you"
-
-if spy_name == "akshit" or spy_name == "Akshit" :
-    print "i love you"
-else :
-    print "fuck off"
